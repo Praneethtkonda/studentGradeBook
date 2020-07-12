685e3f03b2b7718ed402906c96544b69dc43b339
praneethtkonda <praneethtkonda@gmail.com>
2020-07-12 07:36:18 +0000

    Fixed some more bugs related to parsing errors

--- utilities/parser.py
+++ utilities/parser.py
@@ -13,10 +13,13 @@ class Parser():
               3.) Floating point exception
         """
         stud_list = student_string.strip().split(delimiter)
-        if len(stud_list) != 2:
-            raise WrongInputFormat(f"Wrong input student format as it is not in the format {self.stu_format}")
         stud_details_list = []
-        stud_id = stud_list[0].strip()
+        stud_id = stud_list[0].strip().replace(" ", "")
+        dep = stud_id[4:7].replace(" ", "")
+        year = stud_id[0:4].replace(" ", "")
+        # Validating the format
+        if len(stud_list) != 2 or len(stud_id) != 11 or len(dep) != 3 or year.isdecimal() == False or dep.isdecimal():
+            raise WrongInputFormat(f"Wrong input student format as it is not in the format {self.stu_format} for {student_string}")
         stud_cgpa = float(stud_list[1].strip())
         if not 0.0 <= stud_cgpa <= 5.0:
             raise CgpaNotInRangeError(stud_cgpa)
@@ -39,14 +42,17 @@ class Parser():
                 final_list.append(val)
         except WrongInputFormat as e:
             print(f"ERROR: {e.args[0]} at file:- {file_name}")
+            final_list = []
         except ValueError:
-            print("ERROR: Couldnot parse the file {file_name} because of floating point conversion")
+            print(f"ERROR: Could not parse the file {file_name} because of floating point conversion")
+            final_list = []
         except CgpaNotInRangeError as e:
             print(f"CGPA {e.args[0]} not in range at file:- {file_name}")
             # Setting this will not insert any record
             final_list = []
         except IOError:
             print('Error while reading/opening into the file :- {file_name}')
+            final_list = []
         finally:
             file.close()
             return final_list
@@ -73,7 +79,7 @@ class Parser():
                 raise CgpaNotInRangeError(max_cgpa)
             output_tuple = (min_cgpa, max_cgpa)
         except ValueError:
-            print(f"ERROR: Couldnot parse the file {file_name} because of floating point conversion")
+            print(f"ERROR: Could not parse the file {file_name} because of floating point conversion")
         except IOError:
             print('ERROR: Cannot find the file or read data')
         except WrongInputFormat as e:
